a24b49e7dfc9a347874e225f31f1a152de1909e4
Max Zakzook <mzakzook@me.com>
2019-11-12 12:33:29 -0800

    Pseudo Code

---
+++ play-project/play-frontend/src/index.js
@@ -0,0 +1,19 @@
+// const BACKEND_URL = 'http://localhost:3000';
+// fetch(`${BACKEND_URL}/games`)
+//   .then(response => 
+//     response.json())
+//   .then(parsedResponse => {
+//     let referenceVar = parsedData
+//     console.log(parsedResponse));
+
+
+//   side_button.addEventlistener('click', function(event) {
+//     let game_type = event.target.textContent
+//     let gameContain = document.querySelector('.games_container')
+//     games = referenceVar.filter(game => game.table.table_type === game_type && game.full === 'false')
+//     gameContain.innerHTML = '<div>
+    
+    
+    
+//     </div>'
+//   })
