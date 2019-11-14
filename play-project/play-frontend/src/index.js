8e93c16efcfd21328816072e843146faf67fec23
Max Zakzook <mzakzook@me.com>
2019-11-13 16:12:31 -0800

    Build join, single entry

--- play-project/play-frontend/src/index.js
+++ play-project/play-frontend/src/index.js
@@ -3,7 +3,7 @@
   const BACKEND_URL = 'http://localhost:3000';
 
   let mainDiv = document.querySelector('main')
-
+  let current_player = {}
   let gamesReference
   let playersReference
 
@@ -27,11 +27,14 @@
   leaderButton.className = "other-button" 
   leaderButton.textContent = "Leaderboard" 
 
-  const sideNavDiv = document.querySelector('.side-nav')
-  sideNavDiv.append(pongButton, foosballButton, shuffleboardButton, liveChatButton, leaderButton)
-  
+  const topDiv = document.querySelector('.top')
+
+  const signInForm = document.querySelector('.user-form')
+
+
   listenForGameSelection()
   listenForJoin()
+  listenForSignIn()
 
   fetch(`${BACKEND_URL}/games`)
     .then(response => 
@@ -47,17 +50,53 @@
     playersReference = data
   })
 
-  // fetch(`${BACKEND_URL}/tables`)
-  // .then(response => 
-  //   response.json())
-  // .then(data => {
-  //   gamesReference = data
-  // })
+
+
+  function ifSignedIn() {
+    topDiv.append(pongButton, foosballButton, shuffleboardButton, liveChatButton, leaderButton)
+    current_player
+    
+    mainDiv.innerHTML = `
+    <h1>Hey ${current_player.name}</h1>
+    <h2>What do you want to Play?</h2>
+    `
+  }
+
+  function listenForSignIn() {
+    signInForm.addEventListener('submit', event => {
+      event.preventDefault()
+      const userInput = event.target.username.value
+      const existingUser = playersReference.data.find(player => player.attributes.name === userInput)
+      if (existingUser) {
+        current_player.id = existingUser.id
+        current_player.name =  existingUser.attributes.name
+        ifSignedIn()
+      } else {
+        fetch(`${BACKEND_URL}/players`, {
+          method: 'POST',
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+          },
+          body: JSON.stringify({
+            name: userInput
+          })
+        })
+        .then(res => res.json())
+        .then(data => {
+          current_player.id = data.id
+          current_player.name = data.name
+          ifSignedIn()
+        })
+      }
+    })
+  }
+
+
 
 
   function listenForGameSelection() {
-    sideNavDiv.addEventListener('click', event => {
-      
+    topDiv.addEventListener('click', event => {
       if (event.target.textContent === "Ping Pong" || event.target.textContent === "Shuffleboard" || event.target.textContent === "Foosball") {
         let game_type = event.target.textContent
         let games = gamesReference.data.filter(game => game.attributes.table.table_type === game_type && game.attributes.full === false)
@@ -67,7 +106,6 @@
   }
 
   function renderGame(game) {
-
     return `<div class="game-div" data-id="game-div-${game.id}">
     <h4>${game.attributes.num_players / 2} v ${game.attributes.num_players / 2}</h4>
     <button data-id="${game.id}"> Join </button>
@@ -87,20 +125,96 @@
         let chosenGame = gamesReference.data.find(game => {
           return game.id === event.target.dataset.id
         })
-debugger
         if (chosenGame.attributes.players.length < chosenGame.attributes.num_players) {
-          createPlayerGame(chosenGame)
+          let playersUl = event.target.nextElementSibling
+          let playersList = playersUl.querySelectorAll("li")
+          let existingName = false
+          for(let i = 0; i < playersList.length; i++) {
+            if (playersList[i].textContent === current_player.name) {
+              existingName = true
+            }
+          }
+          // playersList.find(player => player.textContent === current_player.name)
+          if (!existingName) {
+            
+            if (chosenGame.attributes.players.length === chosenGame.attributes.num_players - 1) {
+              createPlayerGame(chosenGame)
+              // need to build - updateFullValue(chosenGame)
+              renderNewGame(chosenGame)
+            } else {
+              createPlayerGame(chosenGame)
+            }
+            
+          }
         } else {
           alert("Game is full. Try again later.")
         }
+      } else if (event.target.textContent === " Leave ") {
+        deletePlayerGame(event.target)
       }
     })
   }
 
+  function deletePlayerGame(leaveButton) {
+    fetch(`${BACKEND_URL}/player_games/${leaveButton.dataset.id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+    })
+    .then(res => {
+      const playersUl = leaveButton.nextElementSibling
+      const playersListItems = playersUl.querySelectorAll('li')
+      for(let i = 0; i < playersListItems.length; i++) {
+        if (playersListItems[i].textContent === current_player.name) {
+          playersListItems[i].remove()
+          leaveButton.textContent = " Join "
+        }
+      }
 
-  // const current_player = playersReference.find(player => {
-  //   player.name === form.name.value
-  // })
+    })
+  }
+
+  function renderNewGame(game) {
+    fetch(`${BACKEND_URL}/games`, {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: JSON.stringify({
+        table_id: game.attributes.table.id,
+        num_players: game.attributes.num_players,
+        full: false
+      })
+    })
+    .then(res => res.json())
+    .then(data => {
+      gamesReference.data.push(data)
+      const replacedGame = document.querySelector(`[data-id="game-div-${game.id}"]`)
+      setTimeout(function(){
+        updateFull(game.id)
+        // replacedGame.dataset.id.split('-')[2]
+        replacedGame.dataset.id = `game-div-${data.id}`
+        replacedGame.querySelector('ul').innerHTML = ""
+      }, 3000)
+      
+    })
+  }
+
+  function updateFull(id) {
+    fetch(`${BACKEND_URL}/games/${id}`, {
+      method: 'PATCH',
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: JSON.stringify({
+        full: true
+      })
+    })
+  }
 
   function createPlayerGame(chosenGame) {
     fetch(`${BACKEND_URL}/player_games`, {
@@ -111,20 +225,22 @@ debugger
       },
       body: JSON.stringify({
         game_id: chosenGame.id,
-        player_id: 2 
+        player_id: current_player.id
       })
     })
     .then(response => response.json())
     .then(pg => {
       const gameChoice = document.querySelector(`[data-id="game-div-${pg.data.attributes.game.id}"]`)
       const ul = gameChoice.querySelector('ul')
+      const gameButton = gameChoice.querySelector('button')
+      gameButton.textContent = " Leave "
+      gameButton.dataset.id = pg.data.id
       ul.innerHTML += renderJoins([pg.data.attributes.player])
       let refGame = gamesReference.data.find(game => parseInt(game.id) === pg.data.attributes.game.id)
       refGame.attributes.players.push(pg.data.attributes.player)
     })
   }
 
-  // pg.data.attributes.game.id
 
 
 })();
